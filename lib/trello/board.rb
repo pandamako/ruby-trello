b0dd51263037e7bd493fdbc2630f31170ada3b38
Artem Bykov <artem.bikov@gmail.com>
2015-04-17 14:17:35 +0300

    change filter

--- lib/trello/board.rb
+++ lib/trello/board.rb
@@ -87,7 +87,7 @@ module Trello
     # This method, when called, can take a hash table with a filter key containing any
     # of the following values:
     #    :filter => [ :none, :open, :closed, :all ] # default :open
-    many :cards, filter: :all
+    many :cards, filter: :none
 
     # Returns all the lists on this board.
     #
